3fe7ff187a622ce1948fc0daeb4d8a47d29766eb
duhowpi <duhowpi@6fc71eea-3096-11de-9cad-a3574e5575ef>
2009-08-23 21:23:09 +0000

    Añadir archivos de la versión 1.0.5

---
+++ trunk/version.bat
@@ -0,0 +1,20 @@
+@echo off
+title Linux Launcher 1.0.5 BETA - Opciones - Versiones Linux disponibles
+cls
+echo Las versiones Linux disponibles en esta version son:
+echo.
+echo BackTrack 3 Final Release
+echo Damm Small Linux 4.4.10
+echo Kubuntu 9.04
+echo Kolibri OS 0.7.5
+echo NimbleX 2008
+echo PuppyLinux 4.2.1
+echo SliTaz 2.0
+echo GeeXboX 1.2.3
+echo Haiku OS Alpha version r32549
+echo Super Ubuntu 9.04
+echo 2/OS 4.00
+echo.
+echo Pulsa una tecla para volver.
+pause > nul
+option.bat
