66390c563153c4b0194236b2199ee3ba9ffd3ada
duhowpi <duhowpi@6fc71eea-3096-11de-9cad-a3574e5575ef>
2009-08-23 21:22:56 +0000

    Añadir archivos de la versión 1.0.5

---
+++ trunk/option.bat
@@ -0,0 +1,34 @@
+@echo off
+title Linux Launcher 1.0.5 BETA - Opciones
+:menu
+cls
+echo Selecciona la opcion que desees hacer:
+echo.
+echo.
+echo      1) Crear un nuevo HDD
+echo      2) Administrar distribuciones Linux
+echo      3) Continuar descargas
+echo      4) Administrar HDD
+echo      5) Linux Launcher Maker
+echo      6) Versiones de distribuciones Linux
+echo      7) Usar CD2USB
+echo      8) Desinstalar Linux Launcher
+echo      9) Volver
+echo      0) Salir
+echo.
+set _ok=
+set /p _ok=Escoje una opcion:
+if "%_ok%" == "1" create.bat
+if "%_ok%" == "2" confdist.bat
+if "%_ok%" == "3" contdown.bat
+if "%_ok%" == "4" manage.bat
+if "%_ok%" == "5" llmm.bat
+if "%_ok%" == "6" version.bat
+if "%_ok%" == "7" goto cd2usb
+if "%_ok%" == "8" Uninstall.bat
+if "%_ok%" == "9" Launcher.bat
+if "%_ok%" == "0" exit
+goto menu
+:cd2usb
+cd .\cd2usb
+cd2usb.bat
